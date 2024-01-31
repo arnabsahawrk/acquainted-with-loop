23b8a0f9474995bc8324fcdeb43e9d53e75a73fe
arnabw <arnabsahawrk@gmail.com>
2024-01-31 13:40:23 +0600

    modified the program

--- reward/serial.js
+++ reward/serial.js
@@ -1,12 +1,15 @@
 let array = [22, 25, 40, 55, 80, 30, 22];
 let target = 80;
-
-while (true) {
+let count = 0;
+while (array.includes(target)) {
+    count++;
     if (array[0] === target) {
         break;
     }
     let num = array.shift();
     array.push(num);
 }
-
-console.log(array);
+if (count === 0)
+    console.log("The Target Not Founded")
+else
+    console.log(array);
